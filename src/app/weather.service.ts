92a2acd334c4e8a66dc60b0334253399d54a3fe1
andrearotesidev <andrearotesi@gmail.com>
2022-11-19 10:29:08 +0100

    Auto refresh on weather conditions
    All weather conditions get refreshed every 30 seconds

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,28 +1,47 @@
-import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
-
+import {Injectable, OnDestroy} from '@angular/core';
+import {Observable, Subject, timer} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {retry, share, switchMap, takeUntil} from 'rxjs/operators';
 
 @Injectable()
-export class WeatherService {
+export class WeatherService implements OnDestroy {
 
   static URL = 'http://api.openweathermap.org/data/2.5';
   static APPID = '5a4b2d457ecbef9eb2a71e480b947604';
   static ICON_URL = 'https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/';
   private currentConditions = [];
+  private stopPolling = new Subject();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    /* Polling mechanism to refresh weather conditions every 30 seconds */
+    timer(1, 30000).pipe(
+        switchMap(() => this.refreshCurrentConditions()),
+        retry(),
+        share(),
+        takeUntil(this.stopPolling)
+    ).subscribe();
+  }
 
-  addCurrentConditions(zipcode: string): void {
-    // Here we make a request to get the curretn conditions data from the API. Note the use of backticks and an expression to insert the zipcode
+  ngOnDestroy() {
+    this.stopPolling.next(true);
+    this.stopPolling.complete();
+  }
+
+  addOrUpdateCurrentConditions(zipcode: string): void {
+    /* If the weather conditions for the given zipcode have already been added,
+    they are simply updated, otherwise the new conditions are added to the list */
     this.http.get(`${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`)
-      .subscribe(data => this.currentConditions.push({zip: zipcode, data: data}) );
+        .subscribe(data => {
+          const condition = this.getCurrentConditions().find(cond => cond.zip === zipcode);
+          condition ? condition.data = data : this.currentConditions.push({zip: zipcode, data: data});
+        });
   }
 
   removeCurrentConditions(zipcode: string) {
-    for (let i in this.currentConditions){
-      if (this.currentConditions[i].zip == zipcode)
+    for (const i in this.currentConditions) {
+      if (this.currentConditions[i].zip === zipcode) {
         this.currentConditions.splice(+i, 1);
+      }
     }
   }
 
@@ -31,26 +50,36 @@ export class WeatherService {
   }
 
   getForecast(zipcode: string): Observable<any> {
-    // Here we make a request to get the forecast data from the API. Note the use of backticks and an expression to insert the zipcode
     return this.http.get(`${WeatherService.URL}/forecast/daily?zip=${zipcode},us&units=imperial&cnt=5&APPID=${WeatherService.APPID}`);
-
   }
 
-  getWeatherIcon(id){
-    if (id >= 200 && id <= 232)
-      return WeatherService.ICON_URL + "art_storm.png";
-    else if (id >= 501 && id <= 511)
-      return WeatherService.ICON_URL + "art_rain.png";
-    else if (id === 500 || (id >= 520 && id <= 531))
-      return WeatherService.ICON_URL + "art_light_rain.png";
-    else if (id >= 600 && id <= 622)
-      return WeatherService.ICON_URL + "art_snow.png";
-    else if (id >= 801 && id <= 804)
-      return WeatherService.ICON_URL + "art_clouds.png";
-    else if (id === 741 || id === 761)
-      return WeatherService.ICON_URL + "art_fog.png";
-    else
-      return WeatherService.ICON_URL + "art_clear.png";
+  refreshCurrentConditions(): Observable<any> {
+    /* Refresh weather data for all current conditions */
+    return new Observable<any>((sub) => {
+      this.getCurrentConditions().forEach(condition => {
+        this.addOrUpdateCurrentConditions(condition.zip);
+        sub.next(condition.data);
+      });
+      sub.complete();
+    });
+  }
+
+  getWeatherIcon(id) {
+    if (id >= 200 && id <= 232) {
+      return WeatherService.ICON_URL + 'art_storm.png';
+    } else if (id >= 501 && id <= 511) {
+      return WeatherService.ICON_URL + 'art_rain.png';
+    } else if (id === 500 || (id >= 520 && id <= 531)) {
+      return WeatherService.ICON_URL + 'art_light_rain.png';
+    } else if (id >= 600 && id <= 622) {
+      return WeatherService.ICON_URL + 'art_snow.png';
+    } else if (id >= 801 && id <= 804) {
+      return WeatherService.ICON_URL + 'art_clouds.png';
+    } else if (id === 741 || id === 761) {
+      return WeatherService.ICON_URL + 'art_fog.png';
+    } else {
+      return WeatherService.ICON_URL + 'art_clear.png';
+    }
   }
 
 }
