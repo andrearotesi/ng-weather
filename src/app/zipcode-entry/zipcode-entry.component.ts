fa659981b7c9fba42fb6e5d78a752fec4887be1b
andrearotesidev <andrearotesi@gmail.com>
2022-11-20 16:25:33 +0100

    Revised country list retrieval

--- src/app/zipcode-entry/zipcode-entry.component.ts
+++ src/app/zipcode-entry/zipcode-entry.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {LocationService} from '../location.service';
-import {observable, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 import {countries, Country} from '../countries.model';
 
 @Component({
@@ -14,20 +14,14 @@ export class ZipcodeEntryComponent {
   country: string = null;
 
   get availableCountries(): string[] {
-    /* Returns the countries that contain the input's value.
-    if the input matches a country from the list, an empty array is returned */
-    if (this.country) {
-      if (this.currentCountry) {
-        return [];
-      }
-      return countries.filter(c => c.description.toLowerCase().includes(this.country.toLowerCase()))
-          .map(c => c.description);
-    }
-    return [];
+    return this.country ?
+        countries.filter(c => c.description.toLowerCase().includes(this.country?.toLowerCase()))
+            .map(c => c.description)
+        : [];
   }
 
   get currentCountry(): Country {
-    return countries.find(c => c.description.toLowerCase() === this.country.toLowerCase());
+    return countries.find(c => c.description.toLowerCase() === this.country?.toLowerCase());
   }
 
   constructor(private service: LocationService) { }
